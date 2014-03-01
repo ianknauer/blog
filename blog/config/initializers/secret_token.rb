cc4add0ba0ed7e3f9e7b84c79d9afc198158a5a5
ianknauer <ian.knauer@gmail.com>
2014-02-28 16:18:09 -0800

    created ui for blog entry and blog index

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c129cff71e2d10e798e5580d99139b657db17f60e19a8c83ed25943e234411d5dda5590711759829a02571ed3ec90ff7ef0aced35c02c181fe65e63ef9dd6225'
---
+++ blog/config/routes.rb
@@ -0,0 +1,3 @@
+Blog::Application.routes.draw do
+get 'ui(/:action)', controller: 'ui'
+end
